e11e11bc1b97ed811af443861a5b34f6d2cee968
galaxy4276 <galaxyhi4276@gmail.com>
2020-10-01 22:12:14 +0900

    study: antd 와 redux 의 더미데이터를 이용한 POST 구현

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,18 +1,64 @@
-import React from 'react';
-import { Card, Image, Button } from 'antd';
+import React, { useState } from 'react';
+import { Card, Button, Popover } from 'antd';
+import { EllipsisOutlined, HeartOutlined, MessageOutlined, RetweetOutlined } from '@ant-design/icons';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import Avatar from 'antd/lib/avatar/avatar';
+import PostImages from './PostImages';
 
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+
+  const id = useSelector((state) => state.user.me?.id);
+  // const id = me?.id;  // me && me.id;
+
   return (
-    <div>
-      <Card>
-        <Image />
-        <Content />
-        <Button></Button>
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          <HeartOutlined key="heart" />,
+          <MessageOutlined key="comment" />,
+          <Popover key="more" content={(
+            <Button.Group>
+              {id && post.User.id === id ? (
+                <>
+                  <Button>수정</Button>
+                  <Button type="danger">삭제</Button>
+                </>)
+              : <Button>신고</Button>}
+            </Button.Group>
+          )}>
+            <EllipsisOutlined />
+          </Popover>
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
       </Card>
+      {/* <CommentForm /> }
+      { <Comments /> */}
     </div>
   );
 };
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.object,
+    Comments: PropTypes.arrayOf(PropTypes.object), // 객체들의 배열
+    Images: PropTypes.arrayOf(PropTypes.object), // 객체들의 배열
+  }).isRequired,
+};
+// shape는 object 요소를 모두 정의
 
 
 export default PostCard;
---
+++ front/components/PostImages.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const PostImages = ({ images }) => {
+  return (
+    <div>
+      구현 중..
+    </div>
+  );
+};
+
+PostImages.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object) 
+};
+
+
+export default PostImages;
